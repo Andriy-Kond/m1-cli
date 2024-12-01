fd9cd015be31d2d437f17bc281bb362c11594f45
Andrii <andrii.kondratovich@gmail.com>
2024-12-01 15:57:00 +0200

    finish task

--- src/contacts.js
+++ src/contacts.js
@@ -1,23 +1,68 @@
 import fs from "fs/promises";
 import path from "path";
+import { nanoid } from "nanoid";
 
 const __dirname = import.meta.dirname;
 const contactsPath = path.join(__dirname, "db", "contacts.json"); // path to file contacts.js
 
 export const readAllContacts = async () => {
-  const contactsBuffer = await fs.readFile(contactsPath);
-  console.log("readAllContacts >> contacts:::", JSON.parse(contactsBuffer));
+  const contactsAsBuffer = await fs.readFile(contactsPath);
+  const contactsAsObject = JSON.parse(contactsAsBuffer);
 
-  // return contacts;
+  return contactsAsObject || null;
 };
 
-export const getAllContacts = async () => {
-  JSON.parse(contactsBookBuffer); // make obj from json
+export const getContactById = async id => {
+  const allContacts = await readAllContacts();
+  const searchedContact = allContacts.find(contact => contact.id === id);
+
+  return searchedContact || null;
 };
 
-readAllContacts();
+export const addNewContact = async (name, email, phone) => {
+  const allContacts = await readAllContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  allContacts.push(newContact);
 
-// export const readContactById = async id => {};
-// export const addNewContact = async (id, name, email, phone) => {};
-// export const editContact = async (id, name, email, phone) => {};
-// export const deleteContact = async id => {};
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return newContact || null;
+};
+
+export const editContact = async data => {
+  const allContacts = await readAllContacts();
+  const idx = allContacts.findIndex(contact => contact.id === data.id);
+  allContacts[idx] = data;
+
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return data || null;
+};
+
+export const deleteContact = async id => {
+  const allContacts = await readAllContacts();
+  const { deletedContact, updatedContacts } = allContacts.reduce(
+    (acc, contact) => {
+      if (contact.id === id) acc.deletedContact = contact;
+      else acc.updatedContacts.push(contact);
+      return acc; // mandatory entry for .reduce()
+    },
+    { deletedContact: null, updatedContacts: [] },
+  );
+  await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+
+  return deletedContact;
+};
+
+// readAllContacts();
+
+// getContactById("qdggE76Jtbfd9eWJHrssH");
+
+// addNewContact("Andrii", "email@mail.com", "555-55-55");
+
+// editContact({
+//   id: "YYvumJ-MTxqtYnOxEsJvT",
+//   name: "Andrii-2",
+//   email: "email@mail.com",
+//   phone: "555-55-66",
+// });
+
+// deleteContact("6zRXyiwh15HG3r0Z9U3y7");
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { program } from "commander";
+import * as dbProcessing from "./contacts.js";
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -14,19 +16,29 @@ const options = program.opts();
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      console.log("readAllContacts()", await dbProcessing.readAllContacts());
       break;
 
     case "get":
-      // ... id
+      console.log("getContactById(id)", await dbProcessing.getContactById(id));
       break;
 
     case "add":
-      // ... name email phone
+      console.log(
+        "addNewContact(name, email, phone)",
+        await dbProcessing.addNewContact(name, email, phone),
+      );
       break;
 
     case "remove":
-      // ... id
+      console.log("deleteContact(id)", await dbProcessing.deleteContact(id));
+      break;
+
+    case "edit":
+      console.log(
+        "editContact(action, id, name, email, phone)",
+        await dbProcessing.editContact(action, id, name, email, phone),
+      );
       break;
 
     default:
